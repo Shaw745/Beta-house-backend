ca625c787c58d1b5d4b5024a84956dc155cd9474
shaw745 <shawbry5150@gmail.com>
2025-08-22 16:08:05 +0100

    first and last name

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,27 +7,46 @@ const User = require("../models/User.js");
 const router = express.Router();
 
 // REGISTER
+// REGISTER
 router.post("/register", async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    // Combine first + last into single name field
-
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
-     const salt = await bcrypt.genSalt(10);
 
-    const hashedPassword = await bcrypt.hash(password, salt );
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ firstName, lastName, email, password: hashedPassword });
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
     await newUser.save();
 
-    res.status(201).json({ message: "House Registration successful" });
+    // create JWT
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(201).json({
+      message: "Registration successful",
+      token,
+      user: {
+        id: newUser._id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+      },
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
 
 // LOGIN
 router.post("/login", async (req, res) => {
